90acd3c1fb25f93cdde7f43f1d500685300781f8
Fabian Beuke <mail@beuke.org>
2016-10-21 15:15:12 +0200

    clean up react highchart lang class

--- src/js/components/LangChart.js
+++ src/js/components/LangChart.js
@@ -1,111 +1,95 @@
 import React from "react";
 import axios from "axios";
 import ReactHighcharts from 'react-highcharts';
-import { map } from 'lodash'
+import { map, first } from 'lodash'
 
 export default class LangChart extends React.Component {
 
-    state = {
+    config = {
         credits: { enabled: false },
         chart: { type: 'spline' },
         title: { text: 'Github Language Statistics' },
-        xAxis: {
-            categories: function() {
-                var q = [];
-                for (var i = 12; i < 99; i++)
-                    for (var n = 1; n < 5; n++)
-                        q.push(i + "/Q" + n)
-                return q;
-            },
-        },
         yAxis: { title: { text: 'Pull Request %' } },
         plotOptions: {
-            spline: { lineWidth: 4, states: {
-                    hover: { lineWidth: 5 } },
+            spline: { lineWidth: 4,
+                states: { hover: { lineWidth: 5 } },
                 marker: { enabled: false },
             }
         },
         tooltip: {
             formatter: function() {
-                return '<span style="color:' + this.series.color + '">' + this.series.name + '</span>: <b>' + Highcharts.numberFormat((this.y * 100), 2, '.') + '%</b>';
+                return '<span style="color:' + this.series.color + '">'
+                + this.series.name + '</span>: <b>'
+                + (this.y * 100).toFixed(2) + '%</b>'
             }
-        },
-    };
+        }
+     };
 
-    constructor(props) {
-        super(props);
-        this.update = this.update.bind(this);
+    isTopLanguage(name) {
+        return _.includes(['C++', 'C', 'Objective-C', 'Ruby', 'Java',
+            'JavaScript', 'Go', 'PHP', 'Python', 'Shell' ], name);
     }
 
-    update(chart) {
-        var xdata = [];
-        axios.get('http://localhost:8080/data.json').then(function(d) {
-            const data = d.data
+    categories() {
+        let q = [];
+        _.map(_.range(12,99), i => {
+            _.map(_.range(1,5), n => { q.push(i + "/Q" + n) })
+        })
+        return q;
+    }
 
-            var langs = ['C++', 'C', 'Objective-C', 'Ruby',
-                'Java', 'JavaScript', 'Go', 'PHP', 'Python', 'Shell'
-            ];
-
-            const langExsist = (name) => {
-                if (!_.includes(langs, name))
-                    return true;
-                for (var key in xdata)
-                    if (xdata[key].name == name)
-                        return true;
-            }
-
-            const addData = (name, d) => {
-                for (var key in xdata)
-                    if (xdata[key].name == name)
-                        (xdata[key].data).push(d)
-            }
-
-            _.map(data, function(d) {
-                if (!(langExsist(d.lang)))
-                    xdata.push({
-                        name: d.lang,
-                        data: []
-                    });
-                addData(d.lang, d.count)
+    sumQuarters(data) {
+        _.mapValues(data, v => {
+            let tmp = [];
+            let a = v.data;
+            _.map(_.range(0, a.length, 3), i => {
+                tmp.push(a[i] + a[i + 1] + a[i + 2]);
             });
-
-            const sumQ = (a) => {
-                var test = [];
-                for (var i = 0; i < a.length; i = i + 3)
-                    test.push(a[i] + a[i + 1] + a[i + 2]);
-                return test;
-            }
-
-            const sumData = (data) => {
-                for (var key in data)
-                    data[key].data = sumQ(data[key].data)
-            }
-
-            const pData = (data) => {
-                for (var i = 0; i < 19; i++) { //TODO fix 19 magic
-                    var temp = 0;
-                    for (var key in data)
-                        temp += data[key].data[i];
-                    for (var key in data)
-                        data[key].data[i] = data[key].data[i] / temp;
-                }
-            }
-
-            sumData(xdata);
-            pData(xdata);
-            // console.log(JSON.stringify(data));
-
-            _.map(xdata, function(d) {
-                chart.addSeries(d, false);
-            });
-            chart.redraw();
+            v.data = tmp;
         });
+        return data;
+    }
 
+    percentageData(data) {
+        _.map(_.range(0, _.first(data).data.length), i => {
+            let tmp = 0;
+            _.mapValues(data, v => tmp += v.data[i]);
+            _.mapValues(data, v => v.data[i] = v.data[i] / tmp);
+        });
+        return data;
+    }
+
+    langExsist(name, d) {
+        for (let key in d) if (d[key].name == name) return true;
+    }
+
+    addData(name, val, d) {
+        _.mapValues(d, v => { if (v.name == name) v.data.push(val) })
+    }
+
+    createSeries({data}) {
+        let tmp = [];
+        _.map(data, ( d => {
+            if (!this.langExsist(d.lang, tmp) && this.isTopLanguage(d.lang))
+                tmp.push({ name: d.lang, data: []});
+            this.addData(d.lang, d.count, tmp)
+        }).bind(this));
+        return tmp;
+    }
+
+    componentDidMount() {
+        let chart = this.refs.chart.getChart();
+        let series = _.flow(this.createSeries,
+            this.sumQuarters, this.percentageData).bind(this);
+        axios.get('http://localhost:8080/data.json').then(( d => {
+            _.map(series(d), s => chart.addSeries(s, false));
+            _.first(chart.xAxis).setCategories(this.categories())
+            chart.redraw();
+        }).bind(this));
     }
 
     render() {
-        return (<ReactHighcharts config = { this.state }
-            callback = { this.update }> </ReactHighcharts>
+        return (<ReactHighcharts config = { this.config } ref="chart"> </ReactHighcharts>
         );
     }
 }
