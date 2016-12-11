b319ba26340c10809d50c3f85383730492e58aca
Fabian Beuke <mail@beuke.org>
2016-12-11 02:53:31 +0100

    move styl to src folder

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
-import styles from './style.styl';
+import styles from '../style.styl';
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
