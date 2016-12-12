d6e7af00ce6aa8b75f3fc9028851982ec16bf86b
Fabian Beuke <mail@beuke.org>
2016-12-12 22:07:30 +0100

    add license pie chart

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import LangChart from "./LangChart"
 import LangTable from "./LangTable"
+import LicensePie from "./LicensePie"
 
 export default class Layout extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class Layout extends React.Component {
       <div>
         <LangChart/>
         <LangTable/>
+        <LicensePie/>
       </div>
     );
   }
---
+++ src/js/components/LicensePie.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from 'axios'
+import data from '../../data/github-licenses.json'
+import ReactHighcharts from 'react-highcharts'
+import { map, first } from 'lodash'
+
+export default class LangChart extends React.Component {
+
+    constructor() {
+        super()
+        this.config = {
+        credits: { enabled: false },
+        chart: { type: 'pie', backgroundColor: 'transparent' },
+        title: { text: 'Top 5 Open Source Licenses' },
+        tooltip: {
+            formatter: function() {
+                    return '<span style="color:' + this.series.color + '">'
+                        + this.point.name + '</span>: <b>'
+                        + (this.percentage).toFixed(2) + '%</b>'
+                }
+            },
+        };
+    }
+
+    componentDidMount() {
+        const chart = this.refs.chart.getChart()
+        axios.get(data).then(d => {
+            const licenses = _.chain(d.data)
+              .split('\n')
+              .map(JSON.parse)
+              .map(d => _.mapKeys(d, (val, key) => key == 'license' ? 'name' : 'y'))
+              .each(o => o.y = Math.floor(o.y))
+              .take(5)
+              .value()
+            chart.addSeries({ data: licenses }, false)
+            chart.redraw()
+        })
+    }
+
+    render() {
+        return (<ReactHighcharts config = { this.config } ref="chart"> </ReactHighcharts>);
+    }
+}
