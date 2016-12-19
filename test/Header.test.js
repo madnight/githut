ad14503ae90283158ed820b297e996efcc02c18e
Fabian Beuke <mail@beuke.org>
2016-12-20 00:42:05 +0100

    add test for header

---
+++ test/Header.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import expect from 'expect.js'
+import Header from '../src/js/components/Header'
+import { shallow } from 'enzyme'
+import _ from 'lodash'
+
+describe('Test Header', () => {
+  it('component import should work', () => {
+    expect(Header).not.to.be.equal(null)
+  })
+
+  it('render should work', () => {
+    shallow(<Header/>)
+  })
+
+  it('header should contain title', () => {
+    const html = shallow(<Header/>).html()
+    expect(html).to.contain('A small place to discover languages in GitHub')
+    expect(html).to.not.contain('A big place to discover languages in GitHub')
+  })
+})
