1c60b2656da75ceb7a9d9c76322f1659bda9916c
Fabian Beuke <mail@beuke.org>
2016-12-22 02:50:53 +0100

    remove unused styles

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -10,7 +10,7 @@ const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  devtool: debug ? 'inline-sourcemap' : null,
+  devtool: debug ? 'cheap-module-eval-source-map' : null,
   entry: './js/client.js',
   module: {
     loaders: [
