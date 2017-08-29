a7a7c625474c117b686151ae6696388efc5fe77a
Fabian Beuke <mail@beuke.org>
2017-08-29 05:16:08 +0200

    take github api lang renamings into account

--- src/js/components/LangTable.js
+++ src/js/components/LangTable.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { filter, toString, omitBy, isNil,
+import { filter, toString, omitBy, isNil, find,
     first, sum, update, isNaN, assign, take,
     includes, reject, pick, map } from 'lodash/fp'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 import { NonLangStore } from '../stores/NonLangStore'
+import { RenameLangStore } from '../stores/RenameLangStore'
 import { observer } from 'mobx-react'
 import { autorun } from 'mobx'
 
@@ -18,12 +19,26 @@ export default class LangTable extends React.Component {
     }
 
     filterDate(data, year, quarter) {
-        const nonLang = new NonLangStore().getConfig()
         return data
           | filter({year: year})
           | filter({quarter: quarter})
           | map(pick(['name', 'count']))
-          | reject(o => includes(o.name)(nonLang.lang))
+    }
+
+    filterNonProgrammingLanguages(data) {
+        const nonLang = new NonLangStore().getConfig()
+        return data
+            | reject(o => includes(o.name)(nonLang.lang))
+    }
+
+    applyLanguageRenamings(data) {
+        const renameLang = new RenameLangStore().getConfig()
+        const rename = (name) => {
+            const r = find(o => includes(name, o.before))(renameLang)
+            return r ? r.after : name
+        }
+        return data
+            | map(update('name')(rename))
     }
 
     trendFormatter(cell, row) {
@@ -71,6 +86,28 @@ export default class LangTable extends React.Component {
           | take(50)
     }
 
+    createTable(date, year, quarter) {
+        const addSortId = i => i
+            | map.convert({'cap':0})((o, i) => assign({id: ++i})(o))
+        return this.filterDate(date, year, quarter)
+            | this.filterNonProgrammingLanguages
+            | this.applyLanguageRenamings
+            | addSortId
+            | this.percentageData
+    }
+
+    mountTable() {
+        const data = this.props.store.getData
+        const hist = this.props.hist.getData
+        if (data.length < 1000) return
+        const {year, quarter} = hist
+        const dec = i => --i | toString
+        const curYearRanking = this.createTable(data, year, quarter)
+        const lastYearRanking = this.createTable(data, dec(year), quarter)
+        const trendRanking = this.getTrend(curYearRanking, lastYearRanking)
+        const langRanking = this.getChange(trendRanking, lastYearRanking)
+        this.setState({data: langRanking})
+    }
 
     percentageData(data) {
         const total = data | map('count') | map(Number) | sum
@@ -78,55 +115,43 @@ export default class LangTable extends React.Component {
     }
 
     componentDidMount() {
-    this.handler = autorun(() => {
-        const data = this.props.store.getData
-        const hist = this.props.hist.getData
-        if (data.length > 1000) {
-            const {year, quarter} = hist
-            const dec = i => --i | toString
-
-            const addSortId = i => i
-                | map.convert({'cap':0})((o, i) => assign({id: ++i})(o))
-
-            const createTable = (d, y, q) =>
-                this.filterDate(d, y, q)
-                    | addSortId
-                    | this.percentageData
-
-            const curYearRanking = createTable(data, year, quarter)
-            const lastYearRanking = createTable(data, dec(year), quarter)
-            const trendRanking = this.getTrend(curYearRanking, lastYearRanking)
-            const langRanking = this.getChange(trendRanking, lastYearRanking)
-            this.setState({data: langRanking})
-        }
-      })
+        autorun(() => this.mountTable())
     }
 
     static propTypes = {
-            store: React.PropTypes.object.isRequired,
-            hist: React.PropTypes.object.isRequired
+        store: React.PropTypes.object.isRequired,
+        hist: React.PropTypes.object.isRequired
     }
 
     percentFormatter(cell, row) {
-        const font = (i, color) => `<font size="1">(<font color="${color}">${i}%</font>)</font>`
-        const colorize = i => i > 0
+        const font = (i, color) =>
+            `<font size="1">(<font color="${color}">${i}%</font>)</font>`
+        const colorize = i => i >= 0
             ? font(('+' + i), "green")
             : font(i, "chrimson")
         const percent = i => ((i * 100).toFixed(3))
         const countPercent = (row.count | percent) + "%"
-        // NaN can happen in case of name changes such as FORTRAN to Fortran ()
-        if (row.id > 30 || isNaN(row.change)) return countPercent
-        return `${ countPercent + "  " + (row.change | percent | colorize) }`
+        // NaN can happen in case of new first seen languages,
+        // hence we say 0% change
+        const normalize = n => isNaN(n) ? 0.000 : n
+        if (row.id > 30 ) return countPercent
+        return `${ countPercent + "  " +
+            (row.change
+            | normalize
+            | percent
+            | colorize) }`
+    }
+
+    noDataAvailableYet() {
+        return (
+            <div className="emptyTable">
+                <h2>No data available for this time period yet</h2>
+            </div>
+        )
     }
 
     render() {
-        if (this.props.store.getData.length < 1000
-            || this.state.data.length < 50)
-            return (
-                <div className="emptyTable">
-                    <h2>No data available for this time period yet</h2>
-                </div>
-            )
+        if (this.state.data.length < 50) return this.noDataAvailableYet()
         return (
             <BootstrapTable
                 condensed
---
+++ src/js/stores/RenameLangStore.js
@@ -0,0 +1,28 @@
+/**
+ * This Store keeps language renamings that can happen
+ * when the github api decides to change the language name,
+ * to keep consistency we always choose the latest name and
+ * replace all older names
+ * @class
+ * @classdesc This Store keeps language renamings
+ */
+export class RenameLangStore {
+
+  constructor() {
+    this.config = {
+        vim : {
+          before: ["VimL"],
+          after: "Vim script"
+        },
+        fortran : {
+          before: ["FORTRAN"],
+          after: "Fortran"
+        }
+      }
+  }
+
+  getConfig() {
+    return this.config;
+  }
+
+}
