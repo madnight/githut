517313db578ee04393eea0ea930e4f967884e47b
Fabian Beuke <mail@beuke.org>
2017-10-02 22:37:33 +0200

    fix big query script

--- scripts/query.js
+++ scripts/query.js
@@ -34,8 +34,7 @@ const queryBuilder = (tables) => {
           SELECT lang as language, y as year, q as quarter, type,
           COUNT(*) as count FROM (SELECT a.type type, b.lang lang, a.y y, a.q q FROM (
           SELECT type, YEAR(created_at) as y, QUARTER(created_at) as q,
-          STRING(REGEXP_REPLACE(repo.url, r'(https:\/\/github\.com\/|
-          https:\/\/api\.github\.com\/repos\/)', '')) as name
+          STRING(REGEXP_REPLACE(repo.url, r'https:\/\/github\.com\/|https:\/\/api\.github\.com\/repos\/', '')) as name
           FROM ${tables} ) a
           JOIN ( SELECT repo_name as name, lang FROM ( SELECT * FROM (
           SELECT *, ROW_NUMBER() OVER (PARTITION BY repo_name ORDER BY lang) as num FROM (
@@ -53,12 +52,12 @@ const queryBuilder = (tables) => {
 const main = async () => {
     param
       .version('1.0.0')
-      .option('-t, --tables <item>',
+      .option('-t, --tables <string>',
         'The GitHub Archive tables that you want query example usage:' +
         'node query.js -t "[githubarchive:day.20130818], [githubarchive:day.20140118]"')
       .parse(process.argv)
 
-    const tables = defaultTo('[githubarchive:day.20130118], [githubarchive:day.20140118]')(param.table)
+    const tables = defaultTo('[githubarchive:day.20130118], [githubarchive:day.20140118]')(param.tables)
     const queries = queryBuilder(tables)
     const numToStrReplacer = (key, value) => isNumber(value) ? JSON.stringify(value) : value
     const stringifyFP = x => y => JSON.stringify(y, x)
