c2539087fa62e3aa3a9b913625797e2ca4067a44
Fabian Beuke <mail@beuke.org>
2017-10-02 23:48:09 +0200

    add gh-deploy script commit/push notice

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,9 @@ import ghPages from 'gulp-gh-pages'
 import confirm from 'inquirer-confirm'
 
 gulp.task('deploy', () => {
-    confirm('are you sure you want to deploy to gh-pages').then(() => {
+    confirm('Keep in mind that you need to commit and push all changes to ' +
+    'the master before deploy, otherwise it will not work\n' +
+    'Are you sure you want to deploy to gh-pages').then(() => {
         return gulp.src('./public/**/*')
         .pipe(ghPages())
     }, () => {
