5a3d9eb7e30d8a8680003350c2c69c3e0909ed45
Fabian Beuke <mail@beuke.org>
2021-05-17 21:46:47 +0200

    Head class -> functional component

--- src/js/components/Head.js
+++ src/js/components/Head.js
@@ -5,33 +5,63 @@
  * @license AGPL-3.0
  */
 
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react"
+import Helmet from "react-helmet"
 
-export default class Application extends React.Component {
-    shouldComponentUpdate (nextProps, nextState) {
-        return false
-    }
-
-    render () {
-        return (
-            <div className="application">
-                <Helmet
-                    title="Github Language Stats"
-                    meta={[ {'charset': 'utf-8'} ]}
-                    link={[
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//cdnjs.cloudflare.com/ajax/libs/react-select/1.2.1/react-select.min.css'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//fonts.googleapis.com/icon?family=Material+Icons'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//allenfang.github.io/react-bootstrap-table/css/react-bootstrap-table-all.min.css'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//fonts.googleapis.com/css?family=Open+Sans:400,300,700'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//unpkg.com/react-virtualized-select/styles.css'},
-                        {'rel': 'stylesheet', 'type': 'text/css', 'href': '//unpkg.com/react-virtualized/styles.css'}
-                    ]}
-                />
-            </div>
-        )
-    }
+export default function Application(props) {
+    return (
+        <div className="application">
+            <Helmet
+                title="Github Language Stats"
+                meta={[{ charset: "utf-8" }]}
+                link={[
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//cdnjs.cloudflare.com/ajax/libs/react-select/1.2.1/react-select.min.css",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//fonts.googleapis.com/icon?family=Material+Icons",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//allenfang.github.io/react-bootstrap-table/css/react-bootstrap-table-all.min.css",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//fonts.googleapis.com/css?family=Open+Sans:400,300,700",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//unpkg.com/react-virtualized-select/styles.css",
+                    },
+                    {
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "//unpkg.com/react-virtualized/styles.css",
+                    },
+                ]}
+            />
+        </div>
+    )
 }
