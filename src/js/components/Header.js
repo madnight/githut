4f666dbd48b96088fc887b384762dc0ed4ac6ffb
Fabian Beuke <mail@beuke.org>
2021-05-17 22:16:34 +0200

    Header class -> functional component

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -6,29 +6,26 @@
  * @license AGPL-3.0
  */
 
-import React from 'react'
-import GitHubForkRibbon from 'react-github-fork-ribbon'
+import React from "react"
+import GitHubForkRibbon from "react-github-fork-ribbon"
 
-export default class Header extends React.Component {
-    shouldComponentUpdate (nextProps, nextState) {
-        return false
-    }
-
-    render () {
-        return (
-            <div id="wrapper">
-                <div id="header">
-                    <h1>Git<b>Hut 2.0</b></h1>
-                    <h2>A small place to discover languages in GitHub</h2>
-                </div>
-                <GitHubForkRibbon
-                    href="//github.com/madnight/githut"
-                    color="black"
-                    target="_blank"
-                    position="right">
-                Fork me on GitHub
-                </GitHubForkRibbon>
+export default function Header() {
+    return (
+        <div id="wrapper">
+            <div id="header">
+                <h1>
+                    Git<b>Hut 2.0</b>
+                </h1>
+                <h2>A small place to discover languages in GitHub</h2>
             </div>
-        )
-    }
+            <GitHubForkRibbon
+                href="//github.com/madnight/githut"
+                color="black"
+                target="_blank"
+                position="right"
+            >
+                Fork me on GitHub
+            </GitHubForkRibbon>
+        </div>
+    )
 }
