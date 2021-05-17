18a998cf01eb30a85fa82e3e738ab2f1a058b5f7
Fabian Beuke <mail@beuke.org>
2021-05-17 21:16:21 +0200

    LangTable class -> functional component

--- src/js/components/LangTable.js
+++ src/js/components/LangTable.js
@@ -7,33 +7,21 @@
  * @license AGPL-3.0
  */
 
-import React from 'react'
-import { filter, toString, omitBy, isNil, find,
-    first, sum, update, isNaN, assign, take,
-    includes, reject, pick, map } from 'lodash/fp'
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
-import { NonLangStore } from '../stores/NonLangStore'
-import { RenameLangStore } from '../stores/RenameLangStore'
-import { observer } from 'mobx-react'
-import { autorun } from 'mobx'
-import Lang from './Lang'
+import React, { useState, useEffect } from "react"
+import { filter, toString, omitBy, isNil, find, first, sum } from "lodash/fp"
+import { update, isNaN, assign, take, includes, reject } from "lodash/fp"
+import { pick, map, pipe } from "lodash/fp"
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table"
+import { NonLangStore } from "../stores/NonLangStore"
+import { RenameLangStore } from "../stores/RenameLangStore"
+import { observer } from "mobx-react"
+import { autorun } from "mobx"
 
-@observer
-export default class LangTable extends Lang {
-    /**
-     * Sets inital state and react inline style
-     * @constructor
-     * @param {Object} props - Contains GitHub data sets
-     */
-    constructor (props) {
-        super(props)
-        this.state = {
-            data: []
-        }
-        this.style = {
-            margin: 'auto',
-            maxWidth: 1160
-        }
+export default observer(function LangTable(props) {
+    const [state, setState] = useState({ data: [] })
+    const style = {
+        margin: "auto",
+        maxWidth: 1160,
     }
 
     /**
@@ -42,11 +30,12 @@ export default class LangTable extends Lang {
      * @param {number} year - Example 2017
      * @param {number} quarter - Example 2
      */
-    filterDate (data, year, quarter) {
-        return data
-            | filter({year: year})
-            | filter({quarter: quarter})
-            | map(pick(['name', 'count']))
+    function filterDate(data, year, quarter) {
+        return pipe(
+            filter({ year: year }),
+            filter({ quarter: quarter }),
+            map(pick(["name", "count"]))
+        )(data)
     }
 
     /**
@@ -56,10 +45,9 @@ export default class LangTable extends Lang {
      * Filters non programming languages
      * @param {Object} data - GitHub api data set
      */
-    filterNonProgrammingLanguages (data) {
+    function filterNonProgrammingLanguages(data) {
         const nonLang = new NonLangStore().getConfig()
-        return data
-            | reject(o => includes(o.name)(nonLang.lang))
+        return reject((o) => includes(o.name)(nonLang.lang))(data)
     }
 
     /**
@@ -68,14 +56,13 @@ export default class LangTable extends Lang {
      * the latest name (replace old names) to keep consistency
      * @param {Object} data - GitHub api data set
      */
-    applyLanguageRenamings (data) {
+    function applyLanguageRenamings(data) {
         const renameLang = new RenameLangStore().getConfig()
         const rename = (name) => {
-            const r = find(o => includes(name, o.before))(renameLang)
+            const r = find((o) => includes(name, o.before))(renameLang)
             return r ? r.after : name
         }
-        return data
-            | map(update('name')(rename))
+        return map(update("name")(rename))(data)
     }
 
     /**
@@ -87,22 +74,23 @@ export default class LangTable extends Lang {
      * @param {Object} cell - Cell content of the table
      * @param {Object} row - Row content of the table
      */
-    trendFormatter (cell, row) {
-        const arrow = n => {
-            const angle = dir => `<i class='fa fa-angle-${dir}'></i>`
+    function trendFormatter(cell, row) {
+        const arrow = (n) => {
+            const angle = (dir) => `<i class='fa fa-angle-${dir}'></i>`
             switch (true) {
-            case n === 0:
-                return ''
-            case n > 3:
-                return angle('double-up')
-            case n < -3:
-                return angle('double-down')
-            case n < 0:
-                return angle('down')
-            case n > 0:
-                return angle('up')
-            default: // direct jump to top 50 and previously unkown
-                return angle('double-up')
+                case n === 0:
+                    return ""
+                case n > 3:
+                    return angle("double-up")
+                case n < -3:
+                    return angle("double-down")
+                case n < 0:
+                    return angle("down")
+                case n > 0:
+                    return angle("up")
+                default:
+                    // direct jump to top 50 and previously unkown
+                    return angle("double-up")
             }
         }
         return `${arrow(cell)}`
@@ -114,11 +102,8 @@ export default class LangTable extends Lang {
      * @param {string} name - Name to search for
      * @returns {Object} Search result
      */
-    findByName (data, name) {
-        return data
-            | filter({ name: name })
-            | first
-            | omitBy(isNil)
+    function findByName(data, name) {
+        return pipe(filter({ name: name }), first, omitBy(isNil))(data)
     }
 
     /**
@@ -127,12 +112,15 @@ export default class LangTable extends Lang {
      * @param {Object} last - GitHub api data set last year
      * @returns {Object} Data set ++ trend diff
      */
-    getTrend (current, last) {
-        return current
-            | map(c => assign({
-                trend: this.findByName(last, c.name).id - c.id
-            })(c))
-            | take(50)
+    function getTrend(current, last) {
+        return pipe(
+            map((c) =>
+                assign({
+                    trend: findByName(last, c.name).id - c.id,
+                })(c)
+            ),
+            take(50)
+        )(current)
     }
 
     /**
@@ -141,12 +129,15 @@ export default class LangTable extends Lang {
      * @param {Object} last - GitHub api data set last year
      * @returns {Object} Data set ++ change as number
      */
-    getChange (current, last) {
-        return current
-            | map(c => assign({
-                change: c.count - this.findByName(last, c.name).count
-            })(c))
-            | take(50)
+    function getChange(current, last) {
+        return pipe(
+            map((c) =>
+                assign({
+                    change: c.count - findByName(last, c.name).count,
+                })(c)
+            ),
+            take(50)
+        )(current)
     }
 
     /**
@@ -158,34 +149,36 @@ export default class LangTable extends Lang {
      * @param {number} quarter - e.g. 2
      * @returns {Object} filtered and indexed data set (table)
      */
-    createTable (date, year, quarter) {
-        const addSortId = i => i
-            | map.convert({'cap': 0})((o, i) => assign({id: ++i})(o))
+    function createTable(date, year, quarter) {
+        const addSortId = map.convert({ cap: 0 })((o, i) =>
+            assign({ id: ++i })(o)
+        )
 
-        return this.filterDate(date, year, quarter)
-            | this.filterNonProgrammingLanguages
-            | this.applyLanguageRenamings
-            | addSortId
-            | this.percentageData
+        return pipe(
+            filterNonProgrammingLanguages,
+            applyLanguageRenamings,
+            addSortId,
+            percentageData
+        )(filterDate(date, year, quarter))
     }
 
     /**
      * Sets ranking table state based on given props (api data)
      * Gets called on componentDidMount and sets react state on prop change
      */
-    mountTable () {
-        const data = this.props.store.getData
-        const hist = this.props.hist.getData
-        const {year, quarter} = hist
-        const dec = i => --i | toString
+    function mountTable() {
+        const data = props.store.getData
+        const hist = props.hist.getData
+        const { year, quarter } = hist
+        const dec = (i) => toString(--i)
 
-        const curYearRanking = this.createTable(data, year, quarter)
-        const lastYearRanking = this.createTable(data, dec(year), quarter)
-        const trendRanking = this.getTrend(curYearRanking, lastYearRanking)
-        const langRanking = this.getChange(trendRanking, lastYearRanking)
+        const curYearRanking = createTable(data, year, quarter)
+        const lastYearRanking = createTable(data, dec(year), quarter)
+        const trendRanking = getTrend(curYearRanking, lastYearRanking)
+        const langRanking = getChange(trendRanking, lastYearRanking)
 
-        this.props.table.set(langRanking)
-        this.setState({data: langRanking})
+        props.table.set(langRanking)
+        setState({ data: langRanking })
     }
 
     /**
@@ -194,20 +187,20 @@ export default class LangTable extends Lang {
      * @param {Object} data - GitHub api data set
      * @returns {Object} Data set with percentage count
      */
-    percentageData (data) {
-        const total = data | map('count') | map(Number) | sum
-        return data | map(update('count')(d => d / total))
+    function percentageData(data) {
+        const total = pipe(map("count"), map(Number), sum)(data)
+        return pipe(map(update("count")((d) => d / total)))(data)
     }
 
     /**
      * Native react function, called on component mount and
      * on every prop change event via mobx autorun
      */
-    componentDidMount () {
-        this.handler = autorun(() => {
-            this.mountTable()
+    useEffect(() => {
+        autorun(() => {
+            mountTable()
         })
-    }
+    }, [])
 
     /**
      * Formatter that applies color, percentage and change from raw
@@ -216,24 +209,27 @@ export default class LangTable extends Lang {
      * @param {Object} row - Row content of the table
      * @returns {Object} Formatted cell content
      */
-    percentFormatter (cell, row) {
+    function percentFormatter(cell, row) {
         const font = (i, color) =>
             `<font size="1">(<font color="${color}">${i}%</font>)</font>`
-        const colorize = i => i >= 0
-            ? font(('+' + i), 'green')
-            : font(i, 'chrimson')
-        const percent = i => ((i * 100).toFixed(3))
-        const countPercent = (row.count | percent) + '%'
+        const colorize = (i) =>
+            i >= 0 ? font("+" + i, "green") : font(i, "chrimson")
+        const percent = (i) => (i * 100).toFixed(3)
+        const countPercent = (row.count | percent) + "%"
         // NaN can happen in case of new first seen languages,
         // hence we say 0% change
-        const normalize = n => isNaN(n) ? 0.000 : n
-        const offset = '\u00A0'.repeat(6)
-        return offset + (row.id > 30 ? countPercent :
-            (`${countPercent + '  ' +
-            (row.change
-                | normalize
-                | percent
-                | colorize)}`))
+        const normalize = (n) => (isNaN(n) ? 0.0 : n)
+        const offset = "\u00A0".repeat(6)
+        return (
+            offset +
+            (row.id > 30
+                ? countPercent
+                : `${
+                      countPercent +
+                      "  " +
+                      (row.change | normalize | percent | colorize)
+                  }`)
+        )
     }
 
     /**
@@ -241,54 +237,56 @@ export default class LangTable extends Lang {
      * still loading
      * @returns {Object} html content for emtpy table
      */
-    noDataAvailableYet () {
+    function noDataAvailableYet() {
         return (
             <div className="emptyTable">
-                <h2>No data available for this time period yet</h2>
+                <h2>No data available for time period yet</h2>
             </div>
         )
     }
 
-    render () {
-        if (this.state.data.length < 50) return this.noDataAvailableYet()
-        return (
-            <div style={this.style}>
-                <BootstrapTable
-                    condensed
-                    striped
-                    tableStyle={{margin: '30px auto 30px auto', width: '70%'}}
-                    data={this.state.data}
-                    bordered={false}
-                    options={this.options}>
-                    <TableHeaderColumn
-                        width='50px'
-                        dataAlign='center'
-                        dataField='id'
-                        isKey={true}>
-                        # Ranking
-                    </TableHeaderColumn>
-                    <TableHeaderColumn
-                        width='150px'
-                        dataAlign="center"
-                        dataField='name'>
-                        Programming Language
-                    </TableHeaderColumn>
-                    <TableHeaderColumn
-                        width='100px'
-                        dataField='count'
-                        dataAlign="left"
-                        dataFormat={ this.percentFormatter }>
-                        Percentage (YoY Change)
-                    </TableHeaderColumn>
-                    <TableHeaderColumn
-                        width='50px'
-                        dataAlign="center"
-                        dataField='trend'
-                        dataFormat={ this.trendFormatter }>
-                        YoY Trend
-                    </TableHeaderColumn>
-                </BootstrapTable>
-            </div>
-        )
-    }
-}
+    if (state.data.length < 50) return noDataAvailableYet()
+    return (
+        <div style={style}>
+            <BootstrapTable
+                condensed
+                striped
+                tableStyle={{ margin: "30px auto 30px auto", width: "70%" }}
+                data={state.data}
+                bordered={false}
+            >
+                <TableHeaderColumn
+                    width="50px"
+                    dataAlign="center"
+                    dataField="id"
+                    isKey={true}
+                >
+                    # Ranking
+                </TableHeaderColumn>
+                <TableHeaderColumn
+                    width="150px"
+                    dataAlign="center"
+                    dataField="name"
+                >
+                    Programming Language
+                </TableHeaderColumn>
+                <TableHeaderColumn
+                    width="100px"
+                    dataField="count"
+                    dataAlign="left"
+                    dataFormat={percentFormatter}
+                >
+                    Percentage (YoY Change)
+                </TableHeaderColumn>
+                <TableHeaderColumn
+                    width="50px"
+                    dataAlign="center"
+                    dataField="trend"
+                    dataFormat={trendFormatter}
+                >
+                    YoY Trend
+                </TableHeaderColumn>
+            </BootstrapTable>
+        </div>
+    )
+})
