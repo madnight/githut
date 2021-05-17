b92e59fe7051f851d346f251bcf114eb000ee328
Fabian Beuke <mail@beuke.org>
2021-05-17 22:08:15 +0200

    Comments class -> functional component

--- src/js/components/Comments.js
+++ src/js/components/Comments.js
@@ -4,31 +4,17 @@
  * @license AGPL-3.0
  */
 
-import React from 'react'
-import ReactDisqusThread from 'react-disqus-comments'
+import React from "react"
+import ReactDisqusThread from "react-disqus-comments"
 
-export default class Comments extends React.Component {
-    constructor () {
-        super()
-        this.style = {
-            margin: 'auto',
-            maxWidth: 760
-        }
-    }
-
-    shouldComponentUpdate (nextProps, nextState) {
-        return false
-    }
-
-    render () {
-        return (
-            <div style={this.style}>
-                <ReactDisqusThread
-                    shortname="githut2"
-                    identifier="githut2"
-                    title="GitHut2 Thread"
-                />
-            </div>
-        )
-    }
+export default function Comments() {
+    return (
+        <div style={{ margin: "auto", maxWidth: 760 }}>
+            <ReactDisqusThread
+                shortname="githut2"
+                identifier="githut2"
+                title="GitHut2 Thread"
+            />
+        </div>
+    )
 }
