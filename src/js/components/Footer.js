016c7dd5aecc8a8bb0d84644884bb6d615fdff8a
Fabian Beuke <mail@beuke.org>
2021-05-17 22:14:42 +0200

    Footer class -> functional component

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,25 +4,23 @@
  * @author Fabian Beuke <mail@beuke.org>
  * @license AGPL-3.0
  */
+import React from "react"
 
-import React from 'react'
-
-export default class Footer extends React.Component {
-    shouldComponentUpdate (nextProps, nextState) {
-        return false
-    }
-
-    render () {
-        return (
-            <div id="footer">
-                <div id="licence" className="clearfix">
-                        Credits to <a href="//github.com/littleark">Carlo Zapponi</a>
-                    <br/>
-                        This work is licensed under the <a rel="license"
-                        href="//www.gnu.org/licenses/agpl-3.0.en.html" target="_blank">
-                        GNU Affero General Public License</a>
-                </div>
+export default function Footer() {
+    return (
+        <div id="footer">
+            <div id="licence" className="clearfix">
+                Credits to <a href="//github.com/littleark">Carlo Zapponi</a>
+                <br />
+                This work is licensed under the{" "}
+                <a
+                    rel="license"
+                    href="//www.gnu.org/licenses/agpl-3.0.en.html"
+                    target="_blank"
+                >
+                    GNU Affero General Public License
+                </a>
             </div>
-        )
-    }
+        </div>
+    )
 }
