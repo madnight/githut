c14a2bfe368cb9506c05be7e0c7ceb38fe33036f
Fabian Beuke <mail@beuke.org>
2021-05-24 16:03:25 +0200

    yarn audit fix

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -20,8 +20,8 @@ module.exports = {
         loader: "babel-loader"
       },
       {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.md$/,
