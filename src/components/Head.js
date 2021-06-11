8f7ea903af77cdaeea1bbc0d5d07b000d0dd6e67
Fabian Beuke <mail@beuke.org>
2021-06-11 17:05:36 +0200

    remove unused css and inline css

--- src/components/Head.js
+++ src/components/Head.js
@@ -13,53 +13,6 @@ export default function Application() {
             <Helmet
                 title="Github Language Stats"
                 meta={[{ charset: "utf-8" }]}
-                link={[
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//cdnjs.cloudflare.com/ajax/libs/react-select/1.2.1/react-select.min.css",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//fonts.googleapis.com/icon?family=Material+Icons",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//allenfang.github.io/react-bootstrap-table/css/react-bootstrap-table-all.min.css",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//fonts.googleapis.com/css?family=Open+Sans:400,300,700&display=swap",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//unpkg.com/react-virtualized-select/styles.css",
-                    },
-                    {
-                        rel: "stylesheet",
-                        type: "text/css",
-                        href: "//unpkg.com/react-virtualized/styles.css",
-                    },
-                ]}
             />
         </div>
     )
